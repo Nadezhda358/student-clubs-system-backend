806b9b2629bff48d3c2041f060bb8e28578db81d
Nadezhda Todorova <ntodor358@gmail.com>
2026-01-17 19:12:45 +0200

    Initial commit

---
+++ src/main/java/com/school/ppmg/student_clubs_system_api/StudentClubsSystemApiApplication.java
@@ -0,0 +1,13 @@
+package com.school.ppmg.student_clubs_system_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentClubsSystemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentClubsSystemApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/school/ppmg/student_clubs_system_api/StudentClubsSystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.school.ppmg.student_clubs_system_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentClubsSystemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
