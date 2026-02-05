6df8857fdc3500f924123920b5a14774cf75e317
Nadezhda Todorova <ntodor358@gmail.com>
2026-02-05 12:29:38 +0200

    send emails to invite teacher to register

---
+++ src/main/java/com/school/ppmg/student_clubs_system_api/exceptions/ApiExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.school.ppmg.student_clubs_system_api.exceptions;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.time.OffsetDateTime;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class ApiExceptionHandler {
+
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<Map<String, Object>> handle(ResponseStatusException ex) {
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("status", ex.getStatusCode().value());
+        body.put("error", ex.getReason());
+        body.put("timestamp", OffsetDateTime.now().toString());
+        return ResponseEntity.status(ex.getStatusCode()).body(body);
+    }
+}
