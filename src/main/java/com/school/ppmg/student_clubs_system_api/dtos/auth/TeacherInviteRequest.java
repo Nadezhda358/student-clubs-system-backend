bd9b6632d4813eaf35d729f926db944bb1920348
Nadezhda Todorova <ntodor358@gmail.com>
2026-02-22 17:35:31 +0200

    invite multiple teachers

--- src/main/java/com/school/ppmg/student_clubs_system_api/dtos/auth/TeacherInviteRequest.java
+++ src/main/java/com/school/ppmg/student_clubs_system_api/dtos/auth/TeacherInviteRequest.java
@@ -2,7 +2,11 @@ package com.school.ppmg.student_clubs_system_api.dtos.auth;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+
+import java.util.List;
 
 public record TeacherInviteRequest(
-        @Email @NotBlank String email
+        @NotEmpty
+        List<@Email String> emails
 ) {}
