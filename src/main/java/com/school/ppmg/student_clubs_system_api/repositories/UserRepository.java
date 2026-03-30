9a311c3b509bf1e41d022172cb7fdd0f0fde7b58
Nadezhda Todorova <ntodor358@gmail.com>
2026-03-30 23:33:51 +0300

    add AdminTeacherController and TeacherService

--- src/main/java/com/school/ppmg/student_clubs_system_api/repositories/UserRepository.java
+++ src/main/java/com/school/ppmg/student_clubs_system_api/repositories/UserRepository.java
@@ -4,6 +4,7 @@ import com.school.ppmg.student_clubs_system_api.entities.user.User;
 import com.school.ppmg.student_clubs_system_api.enums.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
@@ -13,4 +14,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
 
     long countByRole(UserRole role);
+
+    List<User> findAllByRoleOrderByFirstNameAscLastNameAscIdAsc(UserRole role);
 }
